522bf754144a3aeb8417f71b3f5a8284fdb493f1
QVSD <gameruldragos@gmail.com>
2026-02-25 21:00:38 +0200

    Initial SpringBoot project setup with PostgreSQL (Docker), JPA and Security dependencies

---
+++ README.md
@@ -0,0 +1,7 @@
+# Store Management Tool API
+
+Initial setup:
+- Spring Boot
+- PostgreSQL via Docker
+- JPA & Hibernate
+- Spring Security (to be configured)
---
+++ src/main/java/com/ing/productmng_tool/ProductmngToolApplication.java
@@ -0,0 +1,13 @@
+package com.ing.productmng_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmngToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmngToolApplication.class, args);
+	}
+
+}
