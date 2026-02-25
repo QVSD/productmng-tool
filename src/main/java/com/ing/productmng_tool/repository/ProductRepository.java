b5edfb6b5a6b42fa7ba2ccf10f2df912bd6bf82f
QVSD <gameruldragos@gmail.com>
2026-02-26 00:38:41 +0200

    feat: added ProductRepository

---
+++ src/main/java/com/ing/productmng_tool/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.ing.productmng_tool.repository;
+
+import com.ing.productmng_tool.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    boolean existsByName(String name);
+}
